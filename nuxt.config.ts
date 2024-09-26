d1304f0745ef10303bc5d4fa0d97284dcdabf1d8
MartinsDev <martins.michael71@gmail.com>
2024-09-26 13:01:10 +0100

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "nuxt-security",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    'pinia-plugin-persistedstate/nuxt',
+    "@prisma/nuxt",
+    "@nuxt/icon",
+    "@nuxt/eslint",
+    "nuxt-lodash",
+    'shadcn-nuxt',
+    "nuxt-directus"
+  ],
+  icon: {
+    serverBundle: {
+      collections: ['uil'] // <!--- this
+    }
+  }
+});
